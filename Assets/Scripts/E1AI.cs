8844b9855b2f124bf0aa8eeaa0679ddfb18fe543
MythicalWaffles <85376650+MythicalWaffles@users.noreply.github.com>
2023-09-14 10:52:05 -0400

    Updating Empty Script Files
    I'm pushing everything in chunks so I don't run into the large file transfer issue

--- Assets/Scripts/E1AI.cs
+++ Assets/Scripts/E1AI.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class E1AI : MonoBehaviour
 {
+    GameObject Target;// The Target is the Spacestation
+    float distance; //Distance is the space bewtween the ship and the Spacestation
+    bool inRange = false;//InRange is trueif the spaceship is within a certain distance of the spaceship
+
+    float time;//Basic timer to control how fast the ship shoots
+    Transform rocketSpawn;// Location where the rocket will shoot 
+    GameObject rocket;// Name of rocket projecctile
+
+
+    float speed = 15f;//Speed of the ship
+
+
+    /// <summary>
+    /// Shoot method is a basic method for ship to fire its weapon every 5 seconds
+    /// </summary>
+    void Shoot()
+    {
+        if (time > 5)
+        {
+            time = 0;
+            transform.LookAt(Target.transform);
+            Instantiate(rocket, rocketSpawn.position, rocketSpawn.rotation);
+        }
+    }
+
+    /// <summary>
+    /// Awake is used  to locate the the player's "HomeBase" when the ship spawns
+    /// doing this allows the ships to not sit as "unactivated" game objects in the game and only spawn/take up 
+    /// space when needed
+    /// </summary>
+    void Awake()
+    {
+        Target = GameObject.FindGameObjectWithTag("HomeBase");
+        Debug.Log("Name: " + Target.name);
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+
+
+    /// <summary>
+    /// Just Verication and Adjusting things. 
+    ///
+    /// </summary>
     void Update()
     {
-        
+        float move = speed * Time.deltaTime; //move is made and assigned a value to control how fast the ship moves
+        distance = Vector3.Distance(Target.transform.position, this.transform.position);//distance between the ship is constantly calculated
+
+        if (distance <= 5) // If distance is less than 5, the scripts sets inRange to true which procs another statement and the ship begins to shoot
+        {
+            inRange = true;
+            Shoot();
+        }
+
+        if (!inRange)// if inRange is not false the ship stops moving towardsthe homebase
+        {
+            //Debug.Log("this is being reached");
+            this.transform.position = Vector3.MoveTowards(transform.position, Target.transform.position - new Vector3(20, 0, 0), move);// the stoping distance is an offset of where the HomeBase is.
+        }
+
+        time += Time.deltaTime;//Updating time...
     }
 }
--- Assets/Scripts/SpawnFlow.cs
+++ Assets/Scripts/SpawnFlow.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpawnFlow : MonoBehaviour
 {
+    public TextMeshProUGUI text; //For testing (can be commented out if out)
+    public Transform[] bigShipSpawn; // Array of transforms where big ship spawn
+    public GameObject bigShip; //Big Ship
+
+    public Transform[] smallShipSpawn; //Array of transforms where small ships spawn
+    public GameObject smallShip; //Small Ship
+
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        text.text = "Time: " + timer;
+        if (timer > 10.000f) // Basic set up to spawn ships every 10 seconds
+        {
+            SpawnShips();
+        }
+
+    }
+
+    void SpawnShips()
+    {
+        Debug.Log("Spawning Big Ships");
+        Instantiate(bigShip, bigShipSpawn[0].position, bigShipSpawn[0].rotation);
+        Instantiate(bigShip, bigShipSpawn[1].position, bigShipSpawn[1].rotation);
+        Instantiate(bigShip, bigShipSpawn[2].position, bigShipSpawn[2].rotation);
+
+        Debug.Log("Spawning Small Ships");
+        Instantiate(smallShip, smallShipSpawn[0].position, smallShipSpawn[0].rotation);
+        Instantiate(smallShip, smallShipSpawn[1].position, smallShipSpawn[1].rotation);
+        Instantiate(smallShip, smallShipSpawn[2].position, smallShipSpawn[2].rotation);
+        Instantiate(smallShip, smallShipSpawn[3].position, smallShipSpawn[3].rotation);
+        Instantiate(smallShip, smallShipSpawn[4].position, smallShipSpawn[4].rotation);
+
+        timer = 0;
     }
 }
