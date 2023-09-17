7ac0f5c65c033f8e31214d8ab337aa90e7d2f9f0
MythicalWaffles <85376650+MythicalWaffles@users.noreply.github.com>
2023-09-17 19:46:36 -0400

    Small push - Dion

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MainManager : MonoBehaviour
+{
+    public Slider sfx;
+    public Slider backsfx;
+
+    public GameObject start_bttn;
+
+    public GameObject settings_menu;
+
+    public static MainManager Instance;
+
+    float backgroundVolume;
+    float soundFX;
+
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        settings_menu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        soundFX = sfx.value;
+        backgroundVolume = backsfx.value;
+    }
+
+    public void switch_scenes()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void openSettings()
+    {
+        start_bttn.SetActive(false);
+        settings_menu.SetActive(true);
+    }
+
+    public void closeSettings()
+    {
+        start_bttn.SetActive(true);
+        settings_menu.SetActive(false);
+    }
+
+}
